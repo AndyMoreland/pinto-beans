fffbf964624229484a3fd5fdb61eec9b31189a70
Andrew Moreland <andy@andymo.org>
2013-03-14 15:15:49 -0700

    God damnit Alex.

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -298,10 +298,7 @@ start_process (void *aux)
    exception), returns -1.  If TID is invalid or if it was not a
    child of the calling process, or if process_wait() has already
    been successfully called for the given TID, returns -1
-   immediately, without waiting.
-
-   This function will be implemented in problem 2-2.  For now, it
-   does nothing. */
+   immediately, without waiting. */
 int
 process_wait (tid_t child_tid)
 {
@@ -315,14 +312,7 @@ process_wait (tid_t child_tid)
     pdata_release (child);
     return retval;
   }
-      
-  int counter = 0;
-  while (counter < 100000000) 
-    { 
-      counter++;
-      if (counter % 1000 == 0)
-        counter += 2;
-    }
+
   return -1;
 }
 
